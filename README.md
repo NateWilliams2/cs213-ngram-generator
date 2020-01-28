ab04eec3772efa0ae1ee56ab4acaff8980cca7a8
NateWilliams2 <Williams2@grinnell.edu>
2020-01-27 23:52:27 -0600

    Free dynamic memory

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # cs213-ngram-generator
 
-### Assignment for cs-213 Operating Systems at Grinnell College. 
+### Assignment for cs-213 Operating Systems at Grinnell College.
 The task is to implement a c program which generates all possible substrings of length n from user input.
 
-### Example usage: 
+### Example usage:
 ```$ echo "Some String" | ./ngram N```  
 Note that on some systems the echo command will add a newline character to the end of the string, which will be counted as a character for the purposes of ngrams. To avoid this newline character, run the command as such:  
-```$ echo -e "Some string\c" | ./ngram N```
+```$ echo -n "Some string" | ./ngram N```
--- ngram.c
+++ ngram.c
@@ -29,9 +29,10 @@ int main(int argc, char** argv) {
   return 0;
 }
 
+// Main body of the program. Prints all ngrams of size N from user input.
 void generate_ngrams(int N){
   // Dynamically allocated buffer to hold N-sized stream of chars
-  char* buff = malloc(sizeof(char) * N+1);
+  char* buff = calloc(N+1, sizeof(char));
   char* get_success;
   char newVal[2];
 
@@ -42,19 +43,23 @@ void generate_ngrams(int N){
   }
   // If Nth call to fgets returned NULL, then input string < N so we should exit and print nothing.
   if(get_success == NULL){
+    free(buff);
     return;
   }
 
   // If input is received, proceed through next iteration of loop
   while (get_success != NULL) {
     printf("%s\n", buff);
+    // Get next character, read it into the buffer stream
     get_success = fgets(newVal, 2, stdin);
     rotate_and_add(buff, newVal[0], N);
   }
+  free(buff);
 }
 
+/* rotates buff so that the next character can stream in and all characters are shifted one to the left.
+  N should be the size of buff. The first character is erased.*/
 void rotate_and_add(char* buff, char newVal, int N){
-  //printf("Rotate %s add %d\n", buff, newVal);
   for(int i=0; i<N; i++){
     buff[i] = buff[i+1];
   }
